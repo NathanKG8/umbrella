7e931af7c62bae110f55745b987bec2f2be66fb1
NathanKG8 <164825742+NathanKG8@users.noreply.github.com>
2024-04-08 17:55:05 +0000

    Some progress

--- umbrellamain.rb
+++ umbrellamain.rb
@@ -4,5 +4,13 @@ puts "Will you need an umbrella today?"
 puts "Where are you?"
 locationresponse = gets.chomp
 locationkey = ENV.fetch("GMAPS_KEY")
-apirequest = "https://maps.googleapis.com/maps/api/geocode/json?address=#{locationresponse}&key=#{locationkey}"
-pp apirequest
+googleapirequest = "https://maps.googleapis.com/maps/api/geocode/json?address=#{locationresponse}&key=#{locationkey}"
+pp googleapirequest
+googlecoordinates = HTTP.get(googleapirequest)
+parsed_googlecoordinates = JSON.parse(googlecoordinates)
+pp parsed_googlecoordinates
+results_array = parsed_googlecoordinates.fetch("results")
+address_components_hash = results_array.at(0) 
+geometry_hash = address_components_hash.fetch("geometry")
+location_hash = geometry_hash.fetch("location")
+pp location_hash
