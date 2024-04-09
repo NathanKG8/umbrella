ea80454c49d3181cc925914dab279222f9d62c9e
NathanKG8 <164825742+NathanKG8@users.noreply.github.com>
2024-04-09 01:06:07 +0000

    Finished through temp and next hour

--- umbrellamain.rb
+++ umbrellamain.rb
@@ -5,12 +5,21 @@ puts "Where are you?"
 locationresponse = gets.chomp
 locationkey = ENV.fetch("GMAPS_KEY")
 googleapirequest = "https://maps.googleapis.com/maps/api/geocode/json?address=#{locationresponse}&key=#{locationkey}"
-pp googleapirequest
 googlecoordinates = HTTP.get(googleapirequest)
 parsed_googlecoordinates = JSON.parse(googlecoordinates)
-pp parsed_googlecoordinates
 results_array = parsed_googlecoordinates.fetch("results")
 address_components_hash = results_array.at(0) 
 geometry_hash = address_components_hash.fetch("geometry")
 location_hash = geometry_hash.fetch("location")
-pp location_hash
+latitude = location_hash.fetch("lat")
+longitude = location_hash.fetch("lng")
+weatherkey = ENV.fetch("PIRATE_WEATHER_KEY")
+pirateapirequest = "https://api.pirateweather.net/forecast/#{weatherkey}/#{latitude},#{longitude}"
+weatherresults = HTTP.get(pirateapirequest)
+parsed_weatherresults = JSON.parse(weatherresults)
+currently_hash = parsed_weatherresults.fetch("currently")
+temperature = currently_hash.fetch("temperature")
+puts "It is currently #{temperature} F."
+minute_hash = parsed_weatherresults.fetch("minutely")
+current_status = minute_hash.fetch("summary")
+puts "Next hour: #{current_status}"
